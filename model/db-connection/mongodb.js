b19d9fcdeac753c18fa0353752bcc5a03bbda5cb
ERLEY <erley.1701920254@ucaldas.edu.co>
2023-02-18 17:51:10 -0500

    Comexion de bases de datos

---
+++ model/db-connection/mongodb.js
@@ -0,0 +1,34 @@
+/** packages */
+const mongoose = require("mongoose")
+const config = require("config")
+
+const mongodbInfo = config.get("db-connections.mongodb");
+
+// user: owner_rest-api_user
+// pass: rfRBl5hm71miIhwu
+
+//mongodb+srv://<username>:<password>@cluster0.gcqff9q.mongodb.net/?retryWrites=true&w=majority
+
+const connStr = `mongodb+srv://${mongodbInfo.user}:${mongodbInfo.password}@${mongodbInfo.host}/${mongodbInfo.dbname}?retryWrites=true&w=majority`;
+
+module.exports = () => {
+    mongoose.connect(connStr);
+
+    mongoose.connection.on("connected", () => {
+        console.log("mongodb server connected!")
+    });
+
+    mongoose.connection.on("disconnected", () => {
+        console.log("mongodb server disconnected!")
+    });
+
+    mongoose.connection.on("error", () => {
+        console.log("mongodb server connection error!")
+    });
+
+    mongoose.connection.on("SIGINT", () => {
+        mongoose.connection.close(() => {
+            console.log("mongodb server shutting down!")
+        });
+    });
+};
