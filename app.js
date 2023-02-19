8a30f43ec9bb218cafe697fee6196da337232480
seider sanin <seider.sanin22135@ucaldas.edu.co>
2023-02-19 16:17:38 -0500

    creación de la app.js para funcionamiento

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-"Hola a todos"
+/** packages */
+
+const express = require("express");
+const config = require("config");
+const bodyParser = require("body-parser");
+
+/** app configuration */
+
+const app = express();
+const port = config.get("server-port");
+const jsonParser = bodyParser.json();
+const urlEncodedParser = bodyParser.urlencoded(
+    { extended: true }
+);
+
+app.use(jsonParser);
+app.use(urlEncodedParser);
+
+const ipFn = require("./middlewar/getIpAdress");
+app.use("*", ipFn);
+
+//** methods */
+app.get("/", (req, res, next) => {
+    res.send("welcome to academi rest_api");
+});
+
+const personRoutes = require("./routes/person.routes");
+personRoutes(app);
+const carRoutes = require("./routes/car.routes");
+carRoutes(app);
+
+app.listen(port, () => {
+    console.log("server is running...")
+});
